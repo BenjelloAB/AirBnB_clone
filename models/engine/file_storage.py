e2ed76eae5dbb2735efef09523a640334c39c78a
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2024-01-16 13:43:01 -0800

    fix2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,16 +5,16 @@ from json.decoder import JSONDecodeError
 
 
 class FileStorage:
-    __file_path = "file.json" 
+    __file_path = "file.json"
     __objects = {}
-    
+
     def all(self):
         return FileStorage.__objects
-    
+
     def new(self, obj):
         key_format = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key_format] = obj
-    
+
     def save(self):
         ser = {}
         for key, value in FileStorage.__objects.items():
@@ -22,15 +22,15 @@ class FileStorage:
         with open(FileStorage.__file_path, "w") as f:
             json.dump(ser, f)
 
-    
     def reload(self):
         try:
             des = {}
             with open(FileStorage.__file_path, "r") as f:
                 des = json.loads(f.read())
-            # from models.base_model import BaseModel
+            from models.base_model import BaseModel
             for key, dict_obj in des.items():
-                FileStorage.__objects[key] = eval(dict_obj["__class__"])(**dict_obj)
+                st = dict_obj["__class_"]
+                FileStorage.__objects[key] = eval(st)(**dict_obj)
                 # FileStorage.__objects[key] = BaseModel(**dict_obj)
-        except (FileNotFoundError,JSONDecodeError):
+        except (FileNotFoundError, JSONDecodeError):
             pass
